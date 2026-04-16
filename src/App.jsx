39d4e47af338141733837b9ea8912cca76a8098d
Anuj Hooda <hoodaanuj03@gmail.com>
2026-04-16 14:17:27 +0530

    feat:setting up routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import HomePage from './Pages/HomePage'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <h1 className='text-red-500'>Welcome to My App</h1>
+     <Routes>
+      <Route path='/' element={<HomePage/>}></Route>
+     </Routes>
     </>
   )
 }
