03e37d0d072dd7b0e6d64199ca3239c9236f86cf
Anuj Hooda <hoodaanuj03@gmail.com>
2026-04-22 15:09:30 +0530

    feat:making Best Seller Page

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import MainPage from '../Components/MainPage'
+import BestSeller from '../Components/BestSeller'
 
 const HomePage = () => {
   return (
+    <>
     <div >
       <MainPage/>
     </div>
+    <div >
+      <BestSeller/>
+    </div>
+    </>
   )
 }
 
